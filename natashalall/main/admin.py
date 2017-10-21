9479250204afe0037ea0716eb75b3cf615e71163
Hector Bennett <hectorbennett@outlook.com>
2017-10-21 19:54:56 +0100

    Hector - started working on artwork model

--- natashalall/main/admin.py
+++ natashalall/main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import PageContent
+from .models import Artwork
 
 admin.site.register(PageContent)
+admin.site.register(Artwork)
 
 # Register your models here.
--- natashalall/main/views.py
+++ natashalall/main/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import PageContent
+from .models import Artwork
+
 
 def index(request):
     page_content = PageContent.objects.filter(name='home_text').first()
     return render(request, 'main/index.html', {'page_content': page_content})
 
+
 def contact(request):
     contact_info = PageContent.objects.filter(name='contact_info').first()
     return render(request, 'main/contact.html', {'contact_info': contact_info})
+
+
+def work(request):
+    Artworks = Artwork.objects.filter(
+        date_created__lte=timezone.now()).order_by('date_created')
+    return render(request, 'main/work.html', {'Artworks': Artworks})
