c17c870c808c35d91b95ad81efe04106697967e5
Hector Bennett <hectorbennett@outlook.com>
2017-10-22 11:29:56 +0100

    started separating gallery into a separate app

---
+++ natashalall/galle/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GalleConfig(AppConfig):
+    name = 'galle'
--- natashalall/main/views.py
+++ natashalall/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import PageContent
-from .models import Artwork
+
 
 
 def index(request):
@@ -13,8 +13,3 @@ def contact(request):
     contact_info = PageContent.objects.filter(name='contact_info').first()
     return render(request, 'main/contact.html', {'contact_info': contact_info})
 
-
-def work(request):
-    Artworks = Artwork.objects.filter(
-        date_created__lte=timezone.now()).order_by('date_created')
-    return render(request, 'main/work.html', {'Artworks': Artworks})
