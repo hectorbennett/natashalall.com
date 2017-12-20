18b0ac76b53eb65beae136a7b662a8950cc4089e
Hector Bennett <hectorbennett@outlook.com>
2017-12-20 22:09:36 +0000

    Added multiple images per artwork

--- natashalall/main/admin.py
+++ natashalall/main/admin.py
@@ -2,8 +2,22 @@ from django.contrib import admin
 
 from .models import PageContent
 from .models import Artwork
+from .models import ArtworkImage
+
 # from .models import Shows
 
-admin.site.register(PageContent)
-admin.site.register(Artwork)
+
+# admin.site.register(Artwork)
 # admin.site.register(Shows)
+
+
+# admin.py
+class ArtworkImageInline(admin.TabularInline):
+    model = ArtworkImage
+    extra = 1
+
+class ArtworkAdmin(admin.ModelAdmin):
+    inlines = [ArtworkImageInline,]
+
+admin.site.register(PageContent)
+admin.site.register(Artwork, ArtworkAdmin)
