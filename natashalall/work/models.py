98e07e2bfc89b5a2d5a6b1fc9ab933c60fd88dc1
Hector Bennett <hectorbennett@outlook.com>
2017-12-23 08:27:14 +0000

    Added detail page for artworks

--- natashalall/work/models.py
+++ natashalall/work/models.py
@@ -3,15 +3,17 @@ from django.db import models
 from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFit
 
+
 def image_filename(instance, filename):
     """
-    Used by the ArtworkImage class when uploading image to specify where they are
-    stored and how they are named.
+    Used by the ArtworkImage class when uploading image to specify where they
+    are stored and how they are named.
     """
     ext = filename.split('.')[-1]
     title = instance.get_title()
     filename = "%s.%s" % (title, ext)
-    return os.path.join('img/artwork', filename)
+    return os.path.join('artwork', filename)
+
 
 class Artwork(models.Model):
     title = models.CharField(max_length=100)
@@ -19,6 +21,7 @@ class Artwork(models.Model):
 
     def __str__(self):
         return self.title
+
 
 class ArtworkImage(models.Model):
     artwork = models.ForeignKey(Artwork, related_name='images')
@@ -28,9 +31,9 @@ class ArtworkImage(models.Model):
                                  format='JPEG',
                                  options={'quality': 90})
     image_medium = ImageSpecField(source='image_original',
-                                 processors=[ResizeToFit(300, 300)],
-                                 format='JPEG',
-                                 options={'quality': 90})
+                                  processors=[ResizeToFit(300, 300)],
+                                  format='JPEG',
+                                  options={'quality': 90})
 
     def get_title(self):
         return self.artwork.title
--- natashalall/work/views.py
+++ natashalall/work/views.py
@@ -7,3 +7,8 @@ def index(request):
     artworks = Artwork.objects.filter(
         creation_date__lte=timezone.now()).order_by('creation_date')
     return render(request, 'work/index.html', {'artworks': artworks})
+
+
+def artwork_detail(request, pk):
+    artwork = Artwork.objects.get(pk=pk)
+    return render(request, 'work/artwork_detail.html', {'artwork': artwork})
