071abd00b9391b4af7f240e9d05ada09b963262d
Hector Bennett <hectorbennett@outlook.com>
2018-04-07 19:56:11 +1000

    small mod

--- natashalall/work/views.py
+++ natashalall/work/views.py
@@ -3,10 +3,8 @@ from django.utils import timezone
 from .models import Artwork
 
 
-
 def index(request):
-    artworks = Artwork.objects.filter(
-        creation_date__lte=timezone.now()).order_by('creation_date')
+    artworks = Artwork.objects.all()
     data = {
         'artworks': artworks,
     }
