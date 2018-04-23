8831bd2ee3b30860e688bfac31972ceef180c3d9
Hector Bennett <hectorbennett@outlook.com>
2018-04-23 22:44:04 +0100

    added www.natashalall.com as an allowed_host

--- natashalall/natashalall/settings/common.py
+++ natashalall/natashalall/settings/common.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'natashalall.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'natashalall.com', 'www.natashalall.com']
 
 
 # Application definition
