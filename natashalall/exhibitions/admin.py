460dd0cabb81ed9a458bbf007bbe5efc16fafd06
Hector Bennett <hectorbennett@outlook.com>
2018-04-26 22:02:41 +0100

    Add extra stuff to exhibitions

--- natashalall/exhibitions/admin.py
+++ natashalall/exhibitions/admin.py
@@ -6,5 +6,22 @@ from .models import Exhibition
 class ExhibitionAdmin(admin.ModelAdmin):
     filter_horizontal = ('artworks', )
 
+    fieldsets = (
+        (None, {
+            'fields': (
+                'title',
+                'location',
+                ('date', 'end_date'),
+                'description',
+                'url'
+            )
+        }),
+        ('Artworks', {
+            'fields': (
+                'artworks',
+            )
+        }),
+    )
+
 
 admin.site.register(Exhibition, ExhibitionAdmin)
--- natashalall/exhibitions/models.py
+++ natashalall/exhibitions/models.py
@@ -2,26 +2,60 @@ from django.db import models
 
 
 class Exhibition(models.Model):
-    title = models.CharField(max_length=100)
-    date = models.DateField()
-    description = models.TextField()
+    title = models.CharField(max_length=255)
+    location = models.CharField(
+        max_length=255,
+        blank=True,
+        default=''
+    )
+    date = models.DateField(null=True)
+    end_date = models.DateField('until', null=True, blank=True)
+    description = models.TextField(blank=True)
+    url = models.CharField(
+        max_length=255,
+        help_text='e.g. to the gallery webpage for this exhibition.',
+        blank=True,
+        default=''
+    )
     artworks = models.ManyToManyField(
         'work.Artwork',
         verbose_name='exhibited artworks',
-        help_text="""
-            Artworks can also be added to exhibitions from the artwork edit
-            screen.
-        """
+        help_text=(
+            'Artworks can also be added to exhibitions from the artwork edit '
+            'screen.'
+        ),
+        blank=True
+    )
+    live = models.BooleanField(
+        'is live',
+        default=True,
+        help_text=(
+            'If this is unchecked then this exhibition will not appear '
+            'anywhere on the site.'
+        ),
     )
 
     def __str__(self):
-        return self.title
+        return self.title_location_year
 
     class Meta:
         ordering = ['-date', 'title']
 
-    def _get_title_and_year(self):
-        "Returns the title and year."
-        return ', '.join((str(self.title), str(self.date.year)))
+    @property
+    def title_location_year(self):
+        "Returns title, location, year."
+        fields = (
+            str(self.title),
+            str(self.location),
+            str(self.date.year)
+        )
+        return ', '.join(x for x in fields if x)
 
-    title_and_year = property(_get_title_and_year)
+    @property
+    def title_location(self):
+        "Returns title, location."
+        fields = (
+            str(self.title),
+            str(self.location)
+        )
+        return ', '.join(x for x in fields if x)
