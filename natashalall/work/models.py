dea8a479e0c11ad1e1c0b95a8ea4ddd7b9f89562
Hector Bennett <hectorbennett@outlook.com>
2018-04-26 23:21:22 +0100

    Now fetches youtube cover image

--- natashalall/work/models.py
+++ natashalall/work/models.py
@@ -1,4 +1,6 @@
 import os
+
+from urllib.parse import urlparse, parse_qs
 from django.db import models
 from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFit
@@ -46,21 +48,38 @@ class Artwork(models.Model):
     class Meta:
         ordering = ['-date', 'title']
 
-    def _get_title_and_year(self):
+    @property
+    def title_year(self):
         try:
             return ', '.join((str(self.title), str(self.date.year)))
         except:
             return self.title
 
-    title_and_year = property(_get_title_and_year)
+    @property
+    def thumbnail_url(self):
+        try:
+            tn = self.images.first().image_medium.url
+            if tn:
+                return tn
+        except AttributeError:
+            pass
+
+        try:
+            tn = self.videos.first().thumbnail_url
+            if tn:
+                return tn
+        except AttributeError:
+            pass
+
+        return ''
 
     def __str__(self):
-        return self.title_and_year
+        return self.title_year
 
 
 class ArtworkImage(models.Model):
     artwork = models.ForeignKey(Artwork, related_name='images')
-    image_original = models.ImageField(upload_to=image_filename)
+    image_original = models.ImageField('image', upload_to=image_filename)
     image_large = ImageSpecField(
         source='image_original',
         processors=[ResizeToFit(1500, 1500)],
@@ -83,16 +102,41 @@ class ArtworkImage(models.Model):
 class ArtworkVideo(models.Model):
     artwork = models.ForeignKey(Artwork, related_name='videos')
     url = models.CharField(
+        'youtube url',
         max_length=300,
         help_text=(
-            'Must be in the format https://www.youtube.com/embed/x-xxxxxxxxx'
+            'At the moment this only supports youtube links'
         )
     )
+
+    @property
+    def url_id(self):
+        u_pars = urlparse(self.url)
+        quer_v = parse_qs(u_pars.query).get('v')
+        if quer_v:
+            return quer_v[0]
+        pth = u_pars.path.split('/')
+        if pth:
+            return pth[-1]
+
+    @property
+    def embed_url(self):
+        if self.url_id:
+            return 'https://www.youtube.com/embed/{}'.format(self.url_id)
+        return ''
+
+    @property
+    def thumbnail_url(self):
+        if self.url_id:
+            return 'https://img.youtube.com/vi/{}/hqdefault.jpg'.format(
+                self.url_id)
+        return ''
 
 
 class ArtworkAudio(models.Model):
     artwork = models.ForeignKey(Artwork, related_name='audio_clips')
     url = models.CharField(
+        'soundcloud url',
         max_length=500,
         help_text=(
             'Must be in iframe format. You can find the '
